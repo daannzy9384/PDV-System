8b464f2d4f780ad394cd3bfabf5058307eb76d19
daannzy9384 <danzihodogta5@gmail.com>
2026-05-05 11:24:28 -0300

    ajustes

---
+++ frontend/src/components/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>PDV System</h1>
+      <p>Mercadinho do Dan e Vk</p>
+    </div>
+  );
+};
+
+export default Home;
